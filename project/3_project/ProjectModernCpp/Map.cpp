f4c711def7c29b210f4b5e8be1fd2d0816959ead
alexiapana <alexia.pana@student.unitbv.ro>
2022-11-11 11:26:31 +0200

    definirea clasei map se regaseste si implementarea functiilor de baza ( getter / setter ) + constructor

---
+++ project/3_project/ProjectModernCpp/Map.cpp
@@ -0,0 +1,20 @@
+#include "Map.h"
+
+Map::Map(size_t height, size_t width)
+	: m_height(height)
+	, m_width(width)
+{
+	m_regions.resize(height * width);
+}
+
+const std::optional<Region>& Map::operator[](Position position) const
+{
+	const auto& [line, column] = position;
+	return m_regions[line * m_width + column];
+}
+
+std::optional<Region>& Map::operator[](Position position)
+{
+	const auto& [line, column] = position;
+	return m_regions[line * m_width + column];
+}
