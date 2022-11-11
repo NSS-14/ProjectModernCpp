01adae8e9c0db3475e0848f5fcd32feedde13850
alexiapana <alexia.pana@student.unitbv.ro>
2022-11-11 11:28:01 +0200

    am realizat supraincarcarea operatorului << pentru afiarea mapei.

--- project/3_project/ProjectModernCpp/Map.cpp
+++ project/3_project/ProjectModernCpp/Map.cpp
@@ -18,3 +18,25 @@ std::optional<Region>& Map::operator[](Position position)
 	const auto& [line, column] = position;
 	return m_regions[line * m_width + column];
 }
+
+std::ostream& operator<<(std::ostream& out, const Map& map)
+{
+	std::pair<uint8_t, uint8_t> position;
+	auto& [line, column] = position;
+	for (line = 0; line < map.m_height; line++)
+	{
+		for (column = 0; column < map.m_width; column++)
+		{
+			if (map[position])
+			{
+				out << *map[position];
+			}
+			else
+			{
+				out << 0;
+			}
+		}
+		out << std::endl;
+	}
+	return out;
+}
--- project/3_project/ProjectModernCpp/Map.h
+++ project/3_project/ProjectModernCpp/Map.h
@@ -16,9 +16,13 @@ public:
 	const std::optional<Region>& operator [](Position position) const; //getter
 	std::optional<Region>& operator [](Position position); //setter
 
+public:
+	friend std::ostream& operator <<(std::ostream& out, const Map& map);
+
 private:
 	std::vector<std::optional<Region>> m_regions;
 	std::size_t m_height;
 	std::size_t m_width;
 
 };
+
