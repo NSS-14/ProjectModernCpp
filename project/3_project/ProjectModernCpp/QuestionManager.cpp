fee1760bc4c4ab9c31c9e9ab25557bf787936bab
alexiapana <alexia.pana@student.unitbv.ro>
2022-11-17 19:52:35 +0200

    am implementat functia ReadFile in QuestionManager

--- project/3_project/ProjectModernCpp/QuestionManager.cpp
+++ project/3_project/ProjectModernCpp/QuestionManager.cpp
@@ -8,3 +8,31 @@ QuestionManager::QuestionManager(std::ifstream& in)
 {
 	ReadFile(in);
 }
+
+void QuestionManager::ReadFile(std::ifstream& in)
+{
+	bool type;
+	std::string questionString;
+	int numberOfAnswers;
+	std::vector<std::string> answers;
+
+	while (!in.eof())
+	{
+		in >> type;
+		std::getline(in, questionString);
+		in >> numberOfAnswers;
+		for (int i = 0; i < numberOfAnswers; i++)
+		{
+			std::string answer;
+			in >> answer;
+			answers.push_back(answer);
+		}
+		Question question(questionString, answers);
+		answers.clear();
+		if (type)
+			m_numericalQuestions.push_back(question);
+		else
+			m_gridQuestions.push_back(question);
+	}
+
+}
